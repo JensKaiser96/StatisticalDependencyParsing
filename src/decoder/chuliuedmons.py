9f0e2acda7db01f4c1274d9881b89d0bab24297d
JensKaiser96 <jens-kaiser@online.de>
2022-11-02 14:43:45 +0100

    premature commit

---
+++ src/decoder/chuliuedmons.py
@@ -0,0 +1,17 @@
+from typing import Dict, Any
+
+from networkx import Graph
+import networkx as nx
+
+
+def mst(input_graph: Graph, weigths: Dict[Any, float]) -> Graph:
+    """
+    Chu-Liu-Edmons Algorithm to get the maximal spanning tree for a given graph
+    """
+    return input_graph
+
+
+
+if __name__ == '__main__':
+    mst(None, None)
+
