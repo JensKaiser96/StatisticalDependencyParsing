aa54e7659aa10c85cc11589dfcd0b88035a41ff7
st142204 <ask21D-3414_/!>
2023-02-22 10:45:15 +0100

    changed - iterating over sentences, because old method to get all tokens got removed

--- src/tools/measurements.py
+++ src/tools/measurements.py
@@ -7,12 +7,13 @@ class Measure(ABC):
     def __call__(self, gold_data: TreeBank, test_data: TreeBank) -> float:
         num_correct = 0
         num_tokens = 0
-        for gold_token, test_token in zip(gold_data.tokens, test_data.tokens):
-            if gold_token.id_ != test_token.id_ or gold_token.form != test_token.form:
-                raise TypeError("TreeBanks do not have a matching sequence of tokens/ids")
-            if self.required_equality(gold_token, test_token):
-                num_correct += 1
-            num_tokens += 1
+        for gold_sentence, test_sentence in zip(gold_data, test_data):
+            for gold_token, test_token in zip(gold_sentence, test_sentence):
+                if gold_token.id_ != test_token.id_ or gold_token.form != test_token.form:
+                    raise TypeError("TreeBanks do not have a matching sequence of tokens/ids")
+                if self.required_equality(gold_token, test_token):
+                    num_correct += 1
+                num_tokens += 1
         return num_correct / num_tokens
 
     @staticmethod
