f7439ee25872e04b86c8a044d72bf13273586c0a
st142204 <ask21D-3414_/!>
2023-02-22 10:39:13 +0100

    added - new tests code cleanup

--- test/decoder/chuliuedmonds.py
+++ test/decoder/chuliuedmonds.py
@@ -1,10 +1,7 @@
 import unittest
-from typing import List
 import numpy as np
-# https://github.com/tdozat/Parser-v3/blob/master/scripts/chuliu_edmonds.py
 
 from src.DataStructures.graph import WeightedDirectedGraph as WDG
-from src.DataStructures.graph import ContractedDirectedWeightedGraph as CWDG
 from src.decoder.graph.chuliuedmonds import mst, reduce
 
 
@@ -51,26 +48,33 @@ class ChuLiuEdmondsTest(unittest.TestCase):
         wdg.add_edge(2, 0, 10)
         wdg.add_edge(2, 1, 1)
 
-        print(CWDG(wdg, cycle))
+    def test_nx_conversion(self):
+        wdg = WDG.random(10)
+        gx = wdg.to_nx()
+        wdg_ = WDG.from_nx(gx)
+        assert wdg == wdg_
 
-    def test_resolve_graph(self):
-        pass
-
-
+    def test_mst_one(self):
+        wdg = WDG()
+        wdg.data = np.array(
+            [[0, .31, .18, .20],
+            [ 0,  0, .96, .65],
+            [ 0, .65,  .0, .96],
+            [ 0, .10, .29,  0]]
+        )
+        mst_wdg = mst(wdg)
+        mst_gold = wdg.nx_cle()
+        self.assertEqual(mst_wdg, mst_gold)
 
     def test_mst(self):
-        from test.decoder.chuliuedmonds_n2 import chuliu_edmonds_one_root, flat_tree_to_chonky_boiii
-        wdg = WDG.random(10, seed=12345)
-        scores = wdg.data
-        gold_tree = chuliu_edmonds_one_root(scores)
-        my_tree = mst(wdg).normalize()
-        print(self.flat_tree_to_chonky_boiii(gold_tree))
-
-        print(my_tree)
-        exit(0)
         seed = 12345
         for i in range(2, 100):  # range(2, 20):
-            wdg = WDG.random(i, seed=seed)
-            my_tree = mst(wdg)
-            # print(tree)
-            self.assertTrue(tree.is_well_formed_tree())
+            wdg = WDG.random(i, seed=seed-i)
+            mst_wdg = mst(wdg)
+            mst_gold = wdg.nx_cle()
+            if mst_wdg != mst_gold:
+                print(wdg.data)
+                mst_wdg.draw()
+                mst_gold.draw()
+            self.assertEqual(mst_wdg, mst_gold)
+            print(f"{i} ok")
