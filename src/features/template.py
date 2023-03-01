b6da25cf4ffa31b41902049ffeb10c6478ecf2cf
st142204 <ask21D-3414_/!>
2023-03-01 16:25:59 +0100

    change - removed lemma

--- src/features/template.py
+++ src/features/template.py
@@ -17,18 +17,15 @@ from src.tools.CONLL06 import Token, Sentence, TreeBank
 HEAD = "HEAD"
 DEPE = "DEPE"
 
-LEMMA = "LEMMA"
 FORM = "FORM"
 POS = "POS"
 
 HEAD_FORM = f"{HEAD}_{FORM}"
 HEAD_POS = f"{HEAD}_{POS}"
-HEAD_LEMMA = f"{HEAD}_{LEMMA}"
 DEPE_FORM = f"{DEPE}_{FORM}"
 DEPE_POS = f"{DEPE}_{POS}"
-DEPE_LEMMA = f"{DEPE}_{LEMMA}"
 
-BASIC_FEATURES = (HEAD_FORM, HEAD_POS, HEAD_LEMMA, DEPE_FORM, DEPE_POS, DEPE_LEMMA)
+BASIC_FEATURES = (HEAD_FORM, HEAD_POS, DEPE_FORM, DEPE_POS)
 
 BETW = "BETW"
 NEXT = "NEXT"
@@ -87,8 +84,6 @@ class TemplateWizard:
                     key += f"{feature}_<{token.form}>,"
                 elif DEPE in feature:
                     key += f"{feature}_<{token.pos}>,"
-                elif LEMMA in feature:
-                    key += f"{feature}_<{token.lemma}>,"
             keys.append(key)
         return keys
 
